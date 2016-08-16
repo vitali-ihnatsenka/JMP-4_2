3b5653f455696d2a13fd7a3d93d434e9f899f39c
Vitali Ihnatsenka <vitt.ignat@gmail.com>
2016-08-16 20:09:29 +0300

    task

--- src/com/epam/jmp42/Main.java
+++ src/com/epam/jmp42/Main.java
@@ -3,6 +3,22 @@ package com.epam.jmp42;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //OutOfMemoryError: Java heap space
+        String bigString = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        while(true){
+            bigString += bigString;
+        }
+
+        //StackOverflowError
+  //      methodA();
     }
+
+    private static void methodA(){
+        methodC();
+    }
+
+    private static void methodC(){
+        methodA();
+    }
+
 }
